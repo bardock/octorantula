3d28233f1db694a333479918b8b7e1603a0b8107
Federico Sanchez <fsanchez@originsoftware.com.ar>
2015-01-25 13:14:10 -0300

    initial commit

---
+++ src/core/scraper/app.js
@@ -0,0 +1,32 @@
+﻿var request = require('request')
+  , cheerio = require('cheerio')
+  , async = require('async')
+  , format = require('util').format
+  , endOfLine = require('os').EOL;
+
+var concurrency = 2
+  , url = "https://yts.re/browse-movie/0/All/All/0/latest/";
+
+async.whilst(
+    function () { return url; },
+    function (callback) {
+        request(url, function (err, response, body) {
+            if (err) return callback(err);
+
+            var $ = cheerio.load(body);
+            
+            console.log('----- URL: %s%s', url, endOfLine);
+            
+            $('span.browseTitleLink a').each(function () {
+                console.log('%s%s', $(this).text(), endOfLine);
+            });
+            
+            url = $('.pagination a:contains(Next)').attr("href");
+
+            callback();
+        });
+    },
+    function (err) {
+        if (err) throw err;
+        console.log("DONE!");
+    });
