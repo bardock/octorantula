f5271a649127e66637bf7333100d6dcb1f570e67
Federico Sanchez <fsanchez@originsoftware.com.ar>
2015-01-25 18:33:57 -0300

    scrape movie data

--- src/core/scraper/app.js
+++ src/core/scraper/app.js
@@ -4,8 +4,7 @@
   , format = require('util').format
   , endOfLine = require('os').EOL;
 
-var concurrency = 2
-  , url = "https://yts.re/browse-movie/0/All/All/0/latest/";
+var url = "https://yts.re/browse-movie/0/All/All/0/latest/";
 
 async.whilst(
     function () { return url; },
@@ -17,8 +16,27 @@ async.whilst(
             
             console.log('----- URL: %s%s', url, endOfLine);
             
-            $('span.browseTitleLink a').each(function () {
-                console.log('%s%s', $(this).text(), endOfLine);
+            $('.browse-content').each(function () {
+                var $link = $(this).find("span.browseTitleLink a");
+                var quality = $(this).find(".browseInfoList :contains(Quality:)").parent().contents().eq(1).text().trim();
+                var movie = {
+                    name: $link.text().replace(quality, "").trim(),
+                    size: $(this).find(".browseInfoList :contains(Size:)").parent().contents().eq(1).text().trim(),
+                    genres: $(this).find(".browseInfoList :contains(Genre:)").parent().contents().eq(1).text().trim().split(/[\s|,]+/),
+                    ratings: {
+                        imdb: parseFloat($(this).find(".browseInfoList :contains(IMDB Rating:)").parent().contents().eq(1).text().trim())
+                    },
+                    downloads: {
+                        yify: {
+                            source: $link.attr("href"),
+                            torrent: $(this).find(".torrentDwl").attr("href"),
+                            quality: quality,
+                            peers: $(this).find(".peers").contents().eq(1).text().trim(),
+                            seeds: $(this).find(".seeds").contents().eq(1).text().trim()
+                        }
+                    }
+                };
+                console.log(movie, endOfLine);
             });
             
             url = $('.pagination a:contains(Next)').attr("href");
