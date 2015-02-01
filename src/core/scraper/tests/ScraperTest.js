7c948a8980cff1f2d6ffd370ecffe8445693bb1e
Federico Sanchez <fsanchez@originsoftware.com.ar>
2015-02-01 18:58:32 -0300

    improve unit test

--- src/core/scraper/tests/ScraperTest.js
+++ src/core/scraper/tests/ScraperTest.js
@@ -1,4 +1,5 @@
-var assert = require('assert');
+/// <reference path="../typings/should/should.d.ts" />
+var should = require('should');
 var fs = require('fs');
 var _Scraper = require('../Scraper');
 describe("parseList", function () {
@@ -8,22 +9,21 @@ describe("parseList", function () {
                 throw err;
             var scraper = new _Scraper.Scraper();
             var data = scraper.parseList(html);
-            assert.ok(data);
-            assert.ok(data.nextUrl);
-            assert.ok(data.nextUrl.length > 0);
-            assert.ok(data.movies);
-            assert.ok(data.movies.length == 20);
-            assert.ok(data.movies[0].name == "Man Hunt");
-            assert.ok(data.movies[0].year == 1941);
-            assert.ok(data.movies[0].genres.length == 2);
-            assert.ok(data.movies[0].genres[0] == "Drama");
-            assert.ok(data.movies[0].genres[1] == "Thriller");
-            assert.ok(data.movies[0].rating.imdb == 7.4);
-            assert.ok(data.movies[0].downloads.length == 1);
-            assert.ok(data.movies[0].downloads[0].ripper == "yify");
-            assert.ok(data.movies[0].downloads[0].source == "https://yts.re/movie/man-hunt-1941");
-            assert.ok(data.movies[0].downloads[0].torrent == "https://yts.re/torrent/download/EC70141F8005A5123CEFDFE273AB033FDE177533.torrent");
-            assert.ok(data.movies[0].downloads[0].quality == "1080p");
+            should(data).be.ok;
+            should(data.nextUrl).be.ok.String.and.match(/^https?:\/\//);
+            should(data.movies).be.ok.and.an.Array.of.length(20);
+            data.movies[0].should.be.eql({
+                name: "Man Hunt",
+                year: 1941,
+                genres: ["Drama", "Thriller"],
+                rating: { imdb: 7.4 },
+                downloads: [{
+                    ripper: "yify",
+                    source: "https://yts.re/movie/man-hunt-1941",
+                    torrent: "https://yts.re/torrent/download/EC70141F8005A5123CEFDFE273AB033FDE177533.torrent",
+                    quality: "1080p"
+                }]
+            });
             done();
         });
     });
--- src/core/scraper/tests/ScraperTest.ts
+++ src/core/scraper/tests/ScraperTest.ts
@@ -1,4 +1,6 @@
-﻿import assert = require('assert');
+﻿/// <reference path="../typings/should/should.d.ts" />
+
+import should = require('should');
 import fs = require('fs');
 import _Scraper = require('../Scraper');
 
@@ -8,28 +10,28 @@ describe("parseList", () => {
         
         fs.readFile('yify-list.html', 'utf8', function (err, html) {
             if (err) throw err;
-            
+
             var scraper = new _Scraper.Scraper();
             var data = scraper.parseList(html);
 
-            assert.ok(data);
-            assert.ok(data.nextUrl);
-            assert.ok(data.nextUrl.length > 0);
-            assert.ok(data.movies);
-            assert.ok(data.movies.length == 20);
+            should(data).be.ok;
 
-            assert.ok(data.movies[0].name == "Man Hunt");
-            assert.ok(data.movies[0].year == 1941);
-            assert.ok(data.movies[0].genres.length == 2);
-            assert.ok(data.movies[0].genres[0] == "Drama");
-            assert.ok(data.movies[0].genres[1] == "Thriller");
-            assert.ok(data.movies[0].rating.imdb == 7.4);
-            assert.ok(data.movies[0].downloads.length == 1);
-            assert.ok(data.movies[0].downloads[0].ripper == "yify");
-            assert.ok(data.movies[0].downloads[0].source == "https://yts.re/movie/man-hunt-1941");
-            assert.ok(data.movies[0].downloads[0].torrent == "https://yts.re/torrent/download/EC70141F8005A5123CEFDFE273AB033FDE177533.torrent");
-            assert.ok(data.movies[0].downloads[0].quality == "1080p");
+            should(data.nextUrl).be.ok.String.and.match(/^https?:\/\//);
 
+            should(data.movies).be.ok.and.an.Array.of.length(20);
+
+            data.movies[0].should.be.eql({
+                name: "Man Hunt",
+                year: 1941,
+                genres: ["Drama", "Thriller"],
+                rating: { imdb: 7.4 },
+                downloads: [{
+                    ripper: "yify",
+                    source: "https://yts.re/movie/man-hunt-1941",
+                    torrent: "https://yts.re/torrent/download/EC70141F8005A5123CEFDFE273AB033FDE177533.torrent",
+                    quality: "1080p"
+                }]
+            });
             done();
         });
     });
