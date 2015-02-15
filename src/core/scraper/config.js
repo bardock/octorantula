9ea71157d32ad959eee626766cd2adfefd5cf09e
Federico Sanchez <fsanchez@originsoftware.com.ar>
2015-02-15 20:10:42 -0300

    change parseDetailsInParallel to 4

--- src/core/scraper/config.js
+++ src/core/scraper/config.js
@@ -3,7 +3,7 @@
         level: "debug"
     },
     initUrl: "https://yts.re/browse-movies/0/all/all/0/oldest",
-    parseDetailsInParallel: 3
+    parseDetailsInParallel: 4
 };
 
 module.exports = config;
